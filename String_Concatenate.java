d8d46e2da4556225c048b45d95ab3fc347ee6cd6
mahidhargudipudi <119840056+mahidhargudipudi@users.noreply.github.com>
2023-09-07 11:01:47 +0530

    import java.util.*; public class Temp{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String s1 = sc.next();         String s2 = sc.next();         String s = s1 + s2 ;         char arr[] = s.toCharArray();         Arrays.sort(arr);         String str = new String (arr);         System.out.println(str);     } }

--- String_Concatenate.java
+++ String_Concatenate.java
@@ -1,11 +1,10 @@
 import java.util.*;
 public class Temp{
-    public static void main(String [] args)
-    {
-        Scanner sc = new Scanner (System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
         String s2 = sc.next();
-        String s = s1 + s2 ; 
+        String s = s1 + s2 ;
         char arr[] = s.toCharArray();
         Arrays.sort(arr);
         String str = new String (arr);

29be4432b378dfd072e78ff0b30fd9c8621af595
mahidhargudipudi <119840056+mahidhargudipudi@users.noreply.github.com>
2023-09-08 17:01:41 +0530

    import java.util.*; public class Temp{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         String str[] = s.split(" ");         System.out.println(str.length);
        }
    }

---
+++ words_in_a_string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Temp{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String str[] = s.split(" ");
+        System.out.println(str.length);
+        
+    }
+}
